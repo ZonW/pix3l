021ded56e69a74664b16fd81d27ddc01dbc47348
Zon Wei <zonwei@zons-mbp.myfiosgateway.com>
2022-11-29 17:19:51 -0500

    add styles

--- server/routes/pics.js
+++ server/routes/pics.js
@@ -6,6 +6,8 @@ router.get("/generate", async (req, res) => {
     try{
         let style = req.query.style;
         let text = req.query.text;
+        let validStyles = ['future-architecture-generator', 'fantasy-world-generator', 'stable-diffusion'];
+        if (!validStyles.includes(text)) throw "Style not valid";
         deepai.setApiKey('085f3f96-c9d3-4878-adcb-ca8a8bf279a2');
         var resp = await deepai.callStandardApi(style, {
                 text: text,
