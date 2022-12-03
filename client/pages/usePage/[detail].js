d8b33d6d7c85701adf101083bd34d2b80f358511
XuanlinGuan <xguan2@stevens.edu>
2022-12-03 18:50:24 -0500

    add usepage framwork and dynamic detail framwork

---
+++ client/pages/usePage/[detail].js
@@ -0,0 +1,7 @@
+import React from 'react'
+function Detail(){
+    return <h1> Here is the card detail
+        </h1>
+}
+
+export default Detail;
---
+++ client/pages/usePage/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+function usePage(){
+    return <h1> Here is the usePage</h1>
+}
+
+export default usePage;
